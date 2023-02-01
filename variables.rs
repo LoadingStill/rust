4865e856bade4b29f34e6e5499de2b1017ff2f3e
Ryan Young <youngrya001@gmail.com>
2023-01-31 19:49:23 -0500

    Create variables.rs

---
+++ variables.rs
@@ -0,0 +1,15 @@
+//standard variable
+fn main() {
+    let bunnies = 2;
+}
+
+//32 bit variable
+fn main() {
+    let bunnies: i32 = 4;
+}
+
+
+//multiple variables in the same line
+fn main() {
+    let (bunnies, carrots) = (8, 50);
+}
