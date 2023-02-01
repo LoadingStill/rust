d4333111632c55874f0f673a6919912555d897ab
Ryan Young <youngrya001@gmail.com>
2023-01-31 19:51:09 -0500

    Update variables.rs

--- variables.rs
+++ variables.rs
@@ -1,15 +1,18 @@
-//standard variable - can not change the vaule while in the imudable state, you can change the variable into a mutable state to change (but this is not the default)
+//standard variable
+  //can not change the vaule while in the immutable state, you can change the variable into a mutable state to change (but this is not the default)
 fn main() {
     let bunnies = 2;
 }
 
 //32 bit variable
+  //immutable
 fn main() {
     let bunnies: i32 = 4;
 }
 
 
 //multiple variables in the same line
+  //immutable
 fn main() {
     let (bunnies, carrots) = (8, 50);
 }
